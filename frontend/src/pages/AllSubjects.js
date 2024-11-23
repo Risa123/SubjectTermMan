1ba9604667cf6df8e7aeb381dae79d0b42205542
Ondřej Parchomenko <parchomenkoondra@seznam.cz>
2024-11-23 22:15:04 +0100

    fix

--- frontend/src/pages/AllSubjects.js
+++ frontend/src/pages/AllSubjects.js
@@ -6,7 +6,7 @@ const AllSubjects = () => {
   return (
     <div className='p-8 bg-gray-300 h-screen mt-5'>
     <h1 className='text-2xl p-2 underline font-bold'>Seznam všech předmětů</h1>
-    <List></List>
+    <List />
     </div>
   );
 };
