2823527ac18d8656f4d131294574ffca1f3f16df
Risa123 <risamouris@gmail.com>
2024-11-28 16:43:10 +0100

    database 1

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,15 @@
-const express = require("express")
+const express = require("express");
+const database = require("./database/database")
 
-const PORT = 8000
-const app = express()
-app.use(require("cors")())
-app.use(express.json())
-app.use("/subjectCreate",require("./subjectCreate/subjectCreateRouter"))
-app.use("/subjectGetDetail",require("./subjectGetDetail/subjectGetDetailRouter"))
-app.use("./userExists",require("./userExists/userExistsRouter"))
-app.listen(PORT,() => console.log('server listening on port ${PORT}',PORT))
+const PORT = 8000;
+const app = express();
+app.use(require("cors")());
+app.use(express.json());
+app.use("/subjectCreate",require("./subjectCreate/subjectCreateRouter"));
+app.use("/subjectGetDetail",require("./subjectGetDetail/subjectGetDetailRouter"));
+app.use("./userExists",require("./userExists/userExistsRouter"));
+app.listen(PORT,() =>{
+    database.connect();
+    console.log('server listening on port ${PORT}',PORT);
+});
+process.on("beforeExit",_ => database.close());
