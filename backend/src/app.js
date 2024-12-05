1434a8c691c2b589983ac46231858dba67814ca9
Risa123 <risamouris@gmail.com>
2024-12-05 09:39:54 +0100

    detail renamed

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(require("cors")());
 app.use(express.json());
 app.post("/subject/create",require("./subject/create/route"));
-app.get("/subject/getDetail",require("./subject/getDetail/route"));
+app.get("/subject/get",require("./subject/get/route"));
 app.get("/subject/getList",require("./subject/getList/route"));
 app.post("/user/validate",require("./user/validate/route"));
 app.listen(PORT,() =>{
