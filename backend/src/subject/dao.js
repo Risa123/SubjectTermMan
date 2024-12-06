bd70775e48e8a140266509d375724213c155e9e9
Risa123 <risamouris@gmail.com>
2024-12-06 16:12:49 +0100

    datova vrstva 2

--- backend/src/subject/dao.js
+++ backend/src/subject/dao.js
@@ -13,10 +13,7 @@ async function get(filter){
 async function list(){
   return await getSubjectCollection().find({});
 }
-function update(id,subject){
-  getUserColection().update({_id:id},subject);
-}
-function remove(id){
-  getSubjectCollection().deleteOne({_id:id});
+async function update(id,subject){
+  await getUserColection().update({_id:id},subject);
 }
 module.exports = {create,get,list,update,remove};
